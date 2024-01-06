a770c0b903e56f0266f9966fe4d13fb540119be3
sosow0212 <sosow0212@naver.com>
2024-01-06 17:23:16 +0900

    feat: 프로젝트 설정

---
+++ src/main/java/com/market/ElectronicMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/ElectronicMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
