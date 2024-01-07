540d978d945e107a9ad18639f34271ddc082230f
sosow0212 <sosow0212@naver.com>
2024-01-07 19:18:40 +0900

    refactor: jasypt 중괄호 빠져서 생긴 에러 해결

--- src/main/java/com/market/config/JasyptConfig.java
+++ src/main/java/com/market/config/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password")
+    @Value("${jasypt.encryptor.password}")
     private String ENCRYPT_KEY;
 
     @Bean(name = "jasyptEncryptor")
