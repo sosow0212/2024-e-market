d2a935ee8a8e562d1da19f8b5b2217de132bd34b
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2024-01-07 19:35:35 +0900

    feat: 종속성 변경 및 테스트 자동화 유틸 생성 (#4)
    * chore: querydsl 설정 추가
    
    * refactor: jasypt 중괄호 빠져서 생긴 에러 해결
    
    * chore: mysql 버전 변경 및 스프링 3.x에서도 jasypt 작동되는 버전으로 변경
    
    * refactor: flyway 마이그레이션 false 설정 및 로컬에서 sql 쿼리문 확인할 수 있도록 변경
    
    * refactor: .gitIgnore에 REST Docs, QueryDSL 추가
    
    * feat: 테스트 유틸 추가
    
    * refactor: CI 작동 범위 수정
    
    * refactor: CI 작동 범위 수정

---
+++ src/test/java/com/market/helper/MockBeanInjection.java
@@ -0,0 +1,11 @@
+package com.market.helper;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
+@MockBean(JpaMetamodelMappingContext.class)
+public class MockBeanInjection {
+
+    // @MockBean
+    // protected Example example;
+}
