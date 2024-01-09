0a10db87c79d23da0f9c8cde8f04e22dea4ea51f
sosow0212 <sosow0212@naver.com>
2024-01-09 23:43:11 +0900

    refactor: url 수정

--- src/main/java/com/market/member/ui/auth/AuthController.java
+++ src/main/java/com/market/member/ui/auth/AuthController.java
@@ -3,28 +3,31 @@ package com.market.member.ui.auth;
 import com.market.member.application.auth.AuthService;
 import com.market.member.application.auth.dto.LoginRequest;
 import com.market.member.application.auth.dto.SignupRequest;
+import com.market.member.ui.auth.dto.TokenResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody @Valid final SignupRequest request) {
+    public ResponseEntity<TokenResponse> signup(@RequestBody @Valid final SignupRequest request) {
         String token = authService.signup(request);
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid final LoginRequest request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid final LoginRequest request) {
         String token = authService.login(request);
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 }
