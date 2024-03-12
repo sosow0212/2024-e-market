cf7023e83b17c444fa45674d2c5969c42a70b002
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2024-03-12 15:48:47 +0900

    refactor: 테스트에서 메일을 보내는 문제 해결 (#54)

--- src/test/java/com/market/alarm/application/MailEventHandlerTest.java
+++ src/test/java/com/market/alarm/application/MailEventHandlerTest.java
@@ -3,6 +3,7 @@ package com.market.alarm.application;
 import com.market.helper.IntegrationHelper;
 import com.market.member.application.auth.AuthService;
 import com.market.member.application.auth.dto.SignupRequest;
+import com.market.member.domain.auth.event.RegisteredEvent;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -24,10 +26,13 @@ class MailEventHandlerTest extends IntegrationHelper {
 
     @Test
     void 회원가입_이벤트가_발행되면_구독자가_동작한다() {
+        // given
+        doNothing().when(mailEventHandler).sendMail(any());
+
         // when
         authService.signup(new SignupRequest("email@email.com", "1234"));
 
         // then
-        verify(mailEventHandler).sendMail(any());
+        verify(mailEventHandler).sendMail(any(RegisteredEvent.class));
     }
 }
