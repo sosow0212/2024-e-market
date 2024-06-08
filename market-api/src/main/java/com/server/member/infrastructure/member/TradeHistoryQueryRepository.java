c181ca976ad9edccb3988b6a3a4a31fb01b13879
sosow0212 <sosow0212@naver.com>
2024-06-08 20:00:03 +0900

    refactor: 코드 최적화

--- market-api/src/main/java/com/server/member/infrastructure/member/TradeHistoryQueryRepository.java
+++ market-api/src/main/java/com/server/member/infrastructure/member/TradeHistoryQueryRepository.java
@@ -1,40 +1,46 @@
 package com.server.member.infrastructure.member;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.server.member.domain.member.QMember;
 import com.server.member.domain.member.dto.TradeHistoryResponse;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.function.Function;
 
+import static com.querydsl.core.types.Projections.constructor;
 import static com.server.market.domain.product.QProduct.product;
 import static com.server.member.domain.member.QTradeHistory.tradeHistory;
-import static com.querydsl.core.types.Projections.constructor;
 
 @RequiredArgsConstructor
 @Repository
 public class TradeHistoryQueryRepository {
 
+    private static final String BUYER = "buyer";
+    private static final String SELLER = "seller";
+
     private final JPAQueryFactory jpaQueryFactory;
 
     public List<TradeHistoryResponse> findTradeHistories(final Long memberId, final boolean isSeller) {
         if (isSeller) {
-            return findSellerHistories(memberId);
+            return findHistories(memberId, tradeHistory.sellerId::eq);
         }
 
-        return findBuyerHistories(memberId);
+        return findHistories(memberId, tradeHistory.buyerId::eq);
     }
 
-    private List<TradeHistoryResponse> findSellerHistories(final Long sellerId) {
-        QMember buyer = new QMember("buyer");
-        QMember seller = new QMember("seller");
+    private List<TradeHistoryResponse> findHistories(final Long memberId, final Function<Long, BooleanExpression> memberPredicate) {
+        QMember buyer = new QMember(BUYER);
+        QMember seller = new QMember(SELLER);
 
         return jpaQueryFactory.select(
                         constructor(TradeHistoryResponse.class,
                                 tradeHistory.id,
                                 buyer.nickname,
                                 seller.nickname,
+                                product.id,
                                 product.description.title,
                                 tradeHistory.productOriginPrice,
                                 tradeHistory.productDiscountPrice,
@@ -44,29 +50,7 @@ public class TradeHistoryQueryRepository {
                 .leftJoin(buyer).on(tradeHistory.buyerId.eq(buyer.id))
                 .leftJoin(seller).on(tradeHistory.sellerId.eq(seller.id))
                 .leftJoin(product).on(tradeHistory.productId.eq(product.id))
-                .where(tradeHistory.sellerId.eq(sellerId))
-                .fetch();
-    }
-
-    private List<TradeHistoryResponse> findBuyerHistories(final Long buyerId) {
-        QMember buyer = new QMember("buyer");
-        QMember seller = new QMember("seller");
-
-        return jpaQueryFactory.select(
-                        constructor(TradeHistoryResponse.class,
-                                tradeHistory.id,
-                                buyer.nickname,
-                                seller.nickname,
-                                product.description.title,
-                                tradeHistory.productOriginPrice,
-                                tradeHistory.productDiscountPrice,
-                                tradeHistory.usingCouponIds
-                        )
-                ).from(tradeHistory)
-                .leftJoin(buyer).on(tradeHistory.buyerId.eq(buyer.id))
-                .leftJoin(seller).on(tradeHistory.sellerId.eq(seller.id))
-                .leftJoin(product).on(tradeHistory.productId.eq(product.id))
-                .where(tradeHistory.buyerId.eq(buyerId))
+                .where(memberPredicate.apply(memberId))
                 .fetch();
     }
 }
